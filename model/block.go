427cecbba6ead7c38dc44971e562276e77d390dd
Tomas Lingotti <tomaslingotti@hotmail.com>
2019-08-14 22:55:13 -0300

    adding hash function

--- model/block.go
+++ model/block.go
@@ -1,12 +1,21 @@
 package model
 
-import "time"
+import (
+	"crypto/sha256"
+	"time"
+)
 
 type Block struct {
-	Identifier    string
+	Identifier          string
 	Data          []byte
 	Hash          []byte
 	PrevBlockHash []byte
 	Timestamp     time.Time
 	Header        Header
+}
+
+func GenerateHash(block *Block) string {
+	record := block.Identifier + string(block.Hash) + block.Timestamp.String() + string(block.PrevBlockHash)
+	hashFunc := sha256.New()
+	hashFunc.Write([]byte(record))
 }
