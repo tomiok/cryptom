87028f4f2efd85ae394209389ec23731be9abddc
Tomas Lingotti <tomaslingotti@hotmail.com>
2019-08-19 02:19:23 -0300

    WIP

--- blockchain/blockchainIterator.go
+++ blockchain/blockchainIterator.go
@@ -8,13 +8,14 @@ import (
 
 type BlockChainIterator struct {
 	CurrentHash []byte
+	database *bolt.DB
 }
 
-func (iterator *BlockChainIterator) Next(database *bolt.DB) *model.Block {
+func (iterator *BlockChainIterator) Next() *model.Block {
 
 	var block *model.Block
 
-	err := database.View(func(tx *bolt.Tx) error {
+	err := iterator.database.View(func(tx *bolt.Tx) error {
 		b := tx.Bucket([]byte(db.BlockChainFile))
 		encodedBlock := b.Get(iterator.CurrentHash)
 		block = model.Deserialize(encodedBlock)
